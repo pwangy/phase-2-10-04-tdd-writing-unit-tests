cc3d0179d92f88684148247031535f68b6dc2fe7
pwangy <blipsandclicks@gmail.com>
2024-03-03 17:04:22 -0800

    red and green

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-// Your code here
+export const pointsForWord = (word) => {
+    let points = 0
+    for (const char of word) {
+        if(['a', 'e', 'i', 'o', 'u'].includes(char)) {
+            points += 1
+        } else {
+            points += 2
+        }
+    }
+    return points
+}
+
+// Understand the feature we're building
+// Translate the feature into a test specification
+// Write and implement code that passes the test
+// Clean up and refactor
+// Repeat!
