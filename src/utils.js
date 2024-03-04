2aa3ffd6d280c169942a70db1474d6b1c99f5ed7
pwangy <blipsandclicks@gmail.com>
2024-03-03 17:18:08 -0800

    refactor. red

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,7 @@
 export const pointsForWord = (word) => {
     let points = 0
     for (const char of word) {
-        if(['a', 'e', 'i', 'o', 'u'].includes(char)) {
-            points += 1
-        } else {
-            points += 2
-        }
+        points += /[aeiou]/.test(char) ? 1 : 2
     }
     return points
 }
